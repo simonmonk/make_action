dd5122d619e28b2ca6dcce61236c576bc4175725
Simon Monk <Si@Simons-Mac-Pro.local>
2015-04-28 12:04:03 +0100

    experiemnts 1 to 3

---
+++ python/ex_02_pwm_motor_control.py
@@ -0,0 +1,24 @@
+#ex_02_pwm_motor_control.py
+
+import RPi.GPIO as GPIO
+import time
+
+# Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
+GPIO.setmode(GPIO.BCM)
+
+motor_pin = 18
+
+GPIO.setup(motor_pin, GPIO.OUT)
+motor_pwm = GPIO.PWM(motor_pin, 500)
+
+try:         
+    while True:
+        duty = input('Enter Duty Cycle (0 to 100): ')
+        if duty < 0 or duty > 100:
+            print('0 to 100')
+        else:
+            motor_pwm.ChangeDutyCycle(duty)
+        
+finally:  
+    print("Cleaning up")
+    GPIO.cleanup()
