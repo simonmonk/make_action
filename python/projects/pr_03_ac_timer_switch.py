78754668453a4a6cd3b6ee3943ff5c4863186e40
Simon Monk <Si@Simons-Mac-Pro.local>
2015-06-22 11:59:06 +0100

    project 3

--- python/projects/pr_03_ac_timer_switch.py
+++ python/projects/pr_03_ac_timer_switch.py
@@ -8,30 +8,14 @@ control_pin = 18
 
 GPIO.setup(control_pin, GPIO.OUT)
 
-is_on = False
-
-def input_time(prompt):
-    try:
-        time_str = raw_input(prompt)
-        hr_min = time_str.split(':')
-        on_hour = int(hr_min[0])
-        on_min = int(hr_min[1])
-        return (on_hour, on_min)
-    except:
-        print('Incorrect Time Format (HH:MM)')
-        return (0, 0)
-
 try:         
     while True:
-        on_hour, on_min = input_time('Enter Start Time (HH:MM)')
-        on_hour, on_min = input_time('Enter End Time (HH:MM)')
-        now = datetime.datetime.now()
-        if not is_on and now.hour == on_hour and now.minute == on_min:
-            is_on = True
-            GPIO.write(control_pin, True)
-        if is_on and now.hour == off_hour and now.minute == off_min:
-            is_on = False
-            GPIO.write(control_pin, False)            
+        duration_str = input("On time in minutes: ")
+        duration = int(duration_str) * 60
+        
+        GPIO.output(control_pin, True)
+        time.sleep(duration)
+        GPIO.output(control_pin, False)            
         
 finally:  
     print("Cleaning up")
